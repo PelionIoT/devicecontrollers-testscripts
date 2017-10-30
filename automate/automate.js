dc12a2fd6d7a4fcfc7ebadc18e166bea4f27ab69
bhoopesh <bhoopesh@izuma.net>
2017-10-30 21:20:30 +0530

    test4 on relay

--- automate/automate.js
+++ automate/automate.js
@@ -3,15 +3,14 @@ var colors = require('colors')
 var expect = require('chai').expect
 
 describe('#you have following onboard devices'.yellow, function(){
-	it('list onboard devices compleate', function(done){
-		dev$.select('id=*').listResources().then(function(Resp){
+	dev$.select('id=*').listResources().then(function(Resp){
+		it('list onboard devices compleate', function(done){
 			console.log(Object.keys(Resp))
+			console.log(Object.keys(Resp).length)
 			done()
-			//console.log('<------------------------------------------------------------------------->'.rainbow)
 		})
 	})
-}).then(function(b){
-	dev$.select('id=*').listResources().then(function(a){
-		console.log(Object.keys(a).length)
-	})
+})
+dev$.select('id=*').listResources().then(function(a){
+	console.log(Object.keys(a).length)
 })
