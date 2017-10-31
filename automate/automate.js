ec73dee5a8acce9e264dc5f8b5fa1e26f1a1e90e
bhoopesh <bhoopesh@izuma.net>
2017-10-31 10:54:06 +0530

    test13_on relay

--- automate/automate.js
+++ automate/automate.js
@@ -6,25 +6,28 @@ describe('#you have following onboard devices'.yellow, function(){
 	it('list onboard devices compleate', function(done){
 		dev$.select('id=*').listResources().then(function(Resp){
 			console.log(Object.keys(Resp))
-			console.log(Object.keys(Resp).length)
 			done()
 			//console.log('<------------------------------------------------------------------------->'.rainbow)
 		})
 	})
 })
-
-dev$.select('id=*').listResources().then(function(Resp){
-	dev$.listResourceTypes().then(function(b){
-		len = Object.keys(Resp).length
-		for(var i = 0; i < len; i++){
-			var Resources = Object.keys(Resp)[i]
-			var resourcesTyp = Resp[Resources].type
-			var facades = b[resourcesTyp]['0.0.1'].interfaces
-			console.log(Resources)
-			console.log(resourcesTyp)
-			console.log(facades)
-		}
+describe('#'.yellow, function(){
+	it('list onboard devices compleate', function(done){
+		dev$.select('id=*').listResources().then(function(Resp){
+				len = Object.keys(Resp).length
+				for(var i = 0; i < len; i++){
+					dev$.select('id=*').listResources().then(function(Resp){
+						dev$.listResourceTypes().then(function(b){
+							var Resources = Object.keys(Resp)[i]
+							var resourcesTyp = Resp[Resources].type
+							var facades = b[resourcesTyp]['0.0.1'].interfaces
+							console.log(Resources)
+							console.log(resourcesTyp)
+							console.log(facades)
+					})	})
+				}
+			
+		})
 	})
 })
-
 
