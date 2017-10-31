8543ce3e508f1cf9226334c698ba03c7a87db200
bhoopesh <bhoopesh@izuma.net>
2017-10-31 10:17:53 +0530

    test11_on relay

--- automate/automate.js
+++ automate/automate.js
@@ -16,12 +16,12 @@ describe('#you have following onboard devices'.yellow, function(){
 dev$.select('id=*').listResources().then(function(Resp){
 	len = Object.keys(Resp).length
 	for(var i = 0; i < len; i++){
-		const Resources = Object.keys(a)[i]
-		const resourcesTyp = a[Resources].type
-		//console.log(Resources)
+		const Resources = Object.keys(Resp)[i]
+		const resourcesTyp = Resp[Resources].type
+		console.log(Resources)
 		console.log(resourcesTyp)
-		
-	}
-})
+		//done()
+			}
+		})
 
 
