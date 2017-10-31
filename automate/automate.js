d77b1d5a253f075b5a9904cada5c0159309766f3
bhoopesh <bhoopesh@izuma.net>
2017-10-31 12:40:32 +0530

    test15_on relay

--- automate/automate.js
+++ automate/automate.js
@@ -1,15 +1,13 @@
 var assert = require('assert')
 var colors = require('colors')
 var expect = require('chai').expect
-var setstate = require('./stateproperty.js')
-var getstate = require('./get_device.js')
 
 describe('#you have following onboard devices'.yellow, function(){
 	this.timeout(300000)
 	it('list onboard devices compleate', function(done){
 		dev$.select('id=*').listResources().then(function(Resp){
 			console.log(Object.keys(Resp))
-			console.log(Object.keys(Resp).length)
+			//console.log(Object.keys(Resp).length)
 			done()
 			//console.log('<------------------------------------------------------------------------->'.rainbow)
 		})
@@ -21,32 +19,14 @@ dev$.select('id=*').listResources().then(function(Resp){
 	for(var i = 0; i < len; i++){
 		var Resources = Object.keys(Resp)[i]
 		var resourcesTyp = Resp[Resources].type
-		console.log(Resources)
-		console.log(resourcesTyp)
+		//console.log(Resources)
+		//console.log(resourcesTyp)
 		//for(var j = 0; j < 19; j++){
 			
 				var facades = b[resourcesTyp]['0.0.1'].interfaces[0]
 				console.log(facades)
 			
 		//}
-		if(facades == 'Facades/HasContact'){
-		describe(`#testing ${Resources}...`.yellow,function(done){
-			this.timeout(60000)
-				it(`${Resources} test complete`,function(done){
-					getstate('contact',Resources,facades).then(function(){
-					done()
-						//console.log('----------------------------------------------------------------')
-					},function(err){
-						reject()
-						done()
-					})
-				})
-			
-			
-				
-									
-		})
-	}	
 
 			}
 	})
