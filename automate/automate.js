d0ef400c8ae65ed67a4453642444be475f1e6f18
bhoopesh <bhoopesh@izuma.net>
2017-10-31 11:34:33 +0530

    test13_on relay

--- automate/automate.js
+++ automate/automate.js
@@ -3,6 +3,7 @@ var colors = require('colors')
 var expect = require('chai').expect
 
 describe('#you have following onboard devices'.yellow, function(){
+	this.timeout(300000)
 	it('list onboard devices compleate', function(done){
 		dev$.select('id=*').listResources().then(function(Resp){
 			console.log(Object.keys(Resp))
@@ -15,16 +16,16 @@ describe('#you have following onboard devices'.yellow, function(){
 dev$.select('id=*').listResources().then(function(Resp){
 	len = Object.keys(Resp).length 
 	for(var i = 0; i < len; i++){
-		var Resources = Object.keys(Resp)[i]
-		var resourcesTyp = Resp[Resources].type
+		const Resources = Object.keys(Resp)[i]
+		const resourcesTyp = Resp[Resources].type
 		console.log(Resources)
 		console.log(resourcesTyp)
-		for(var j = 0; j < 19; j++){
+		//for(var j = 0; j < 19; j++){
 			dev$.listResourceTypes().then(function(b){
-				facades = b[resourcesTyp]['0.0.1'].interfaces
+				const facades = b[resourcesTyp]['0.0.1'].interfaces
 				console.log(facades)
 			})
-		}
+		//}
 	}
 })
 	
