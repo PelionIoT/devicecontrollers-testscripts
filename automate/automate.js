36a5a01d0abcc696c27a05bdf12bde0dccb274f2
bhoopesh <bhoopesh@izuma.net>
2017-11-01 12:16:16 +0530

    test18_on relay

--- automate/automate.js
+++ automate/automate.js
@@ -5,80 +5,133 @@ var setstate = require('./stateproperty.js')
 var getstate = require('./get_device.js')
 
 describe('Testing starting......'.yellow.bold,function(){
-		describe('#you have following onboard devices'.yellow, function(){
-			this.timeout(300000)
-			it('list onboard devices compleate', function(done){
-				dev$.select('id=*').listResources().then(function(Resp){
-					console.log(Object.keys(Resp))
-					//console.log(Object.keys(Resp).length)
-					done()
-					//console.log('<------------------------------------------------------------------------->'.rainbow)
-				})
+	describe('#you have following onboard devices'.yellow, function(){
+		this.timeout(300000)
+		it('list onboard devices compleate', function(done){
+			dev$.select('id=*').listResources().then(function(Resp){
+				console.log(Object.keys(Resp))
+				//console.log(Object.keys(Resp).length)
+				done()
+				//console.log('<------------------------------------------------------------------------->'.rainbow)
 			})
 		})
-		dev$.select('id=*').listResources().then(function(Resp){
-			dev$.listResourceTypes().then(function(b){
-				len = Object.keys(Resp).length 
-				for(var i = 0; i < len; i++){
-					var Resources = Object.keys(Resp)[i]
-					var resourcesTyp = Resp[Resources].type
-					for(var j = 0; j < 19; j++){
-						var facades = b[resourcesTyp]['0.0.1'].interfaces[j]
-						//console.log(i +'-' + facades)
-						var regis = Resp[Resources].registered
-						var reach = Resp[Resources].reachable
-						//console.log(regis +'and'.green + reach)
-						if(facades == 'Facades/Switchable'){
-				describe(`#testing ${Resources}...`.yellow,function(){
-					this.timeout(60000)
-						if(regis && reach === true){
-							it(`${Resources} test complete`,function(done){
-							console.log(`device ${Resources} has facades- ${facades}`.blue)
-							console.log('\tdevice:'.green,Resources ,'\n',
-								'\ttesting facades:'.green,facades)
-							setstate('power','on',Resources,facades).then(function() {
-							}, function(err) {
-								it(`${Resources} test fail`,function(done){
-									console.log(`problem in the ${Resources}`)
-									done(new Error("promise is not resolve"));
-								})
-							}).then(function() {
-								setstate('power','off', Resources,facades).then(function() {
-									console.log('Tested Facade:'.green,`${facades} for the device ${Resources} successfully`.blue)
-									done();
-									//resolve
+	})
+	dev$.select('id=*').listResources().then(function(Resp){
+		dev$.listResourceTypes().then(function(b){
+			len = Object.keys(Resp).length 
+			for(var i = 0; i < len; i++){
+				var Resources = Object.keys(Resp)[i]
+				var resourcesTyp = Resp[Resources].type
+				//for(var j = 0; j < 19; j++){
+					var facades = b[resourcesTyp]['0.0.1'].interfaces
+					//console.log(i +'-' + facades)
+					var regis = Resp[Resources].registered
+					var reach = Resp[Resources].reachable
+					//console.log(regis +'and'.green + reach)
+
+					if(facades == 'Facades/Switchable'){
+						describe(`#testing ${Resources}...`.yellow,function(){
+							this.timeout(60000)
+							if(regis && reach === true){
+								it(`${Resources} test complete`,function(done){
+								console.log(`device ${Resources} has facades- ${facades}`.blue)
+								console.log('\tdevice:'.green,Resources ,'\n',
+									'\ttesting facades:'.green,facades)
+								setstate('power','on',Resources,facades).then(function() {
 								}, function(err) {
 									it(`${Resources} test fail`,function(done){
 										console.log(`problem in the ${Resources}`)
-										done(new Error("promise is not resolved"));
+										done(new Error("promise is not resolve"));
 									})
+								}).then(function() {
+									setstate('power','off', Resources,facades).then(function() {
+										console.log('Tested Facade:'.green,`${facades} for the device ${Resources} successfully`.blue)
+										done();
+										//resolve
+									}, function(err) {
+										it(`${Resources} test fail`,function(done){
+											console.log(`problem in the ${Resources}`)
+											done(new Error("promise is not resolved"));
+										})
+									});
 								});
-							});
-							})
-						} 
-						else{
-							it(`${Resources} test fail`,function(done){
-								console.log(`problem in the ${Resources}`)
-								done(new Error("either device is not registered or reachable or both"));
-							})
-						}   
-					})
-				
-			}
-						/*if(facades == 'Facades/Switchable'){
-							describe('this should work',function(){
-								it('this is working properly', function(){
-									console.log('start')
 								})
-							})
-						}
-						else{
-							console.log('debug the issues'.red)
-						}*/
-
+							} 
+							else{
+								it(`${Resources} test fail`,function(done){
+									console.log(`problem in the ${Resources}`)
+									done(new Error("either device is not registered or reachable or both"));
+								})
+							}   
+						})
 					}
-				}
-			})
+					else if(facades == 'Facades/HasBattery'){
+						describe(`#testing ${Resources}...`.yellow,function(){
+						this.timeout(60000)
+							if(regis && reach === true){
+								it(`${Resources} test complete`,function(done){
+									getstate('battery',Resources,facades).then(function(){
+									done()
+								},function(err){
+									reject()
+									done()
+								})
+								})
+							}
+							else{
+								it(`${Resources} test fail`,function(done){
+									console.log(`problem in the ${Resources}`)
+									done(new Error("either device is not registered or reachable or both"));
+								})
+							}   
+						})						
+					}
+					else if(facades == 'Facades/Button'){
+						describe(`#testing ${Resources}...`.yellow,function(){
+							this.timeout(60000)
+							if(regis && reach === true){
+								it(`${Resources} test complete`,function(done){
+									getstate('pressed',Resources,facades).then(function(){
+										done()
+									},function(err){
+										reject()
+										done()
+									})
+								})
+							}
+							else{
+								it(`${Resources} test fail`,function(done){
+									console.log(`problem in the ${Resources}`)
+									done(new Error("either device is not registered or reachable or both"));
+								})
+							}   
+						})						
+					}
+					else if(facades == 'Facades/HasContact'){
+						describe(`#testing ${Resources}...`.yellow,function(done){
+							this.timeout(60000)
+							if(regis && reach === true){
+								it(`${Resources} test complete`,function(done){
+									getstate('contact',Resources,facades).then(function(){
+										done()
+										//console.log('----------------------------------------------------------------')
+									},function(err){
+										reject()
+										done()
+									})
+								})
+							}
+							else{
+								it(`${Resources} test fail`,function(done){
+									console.log(`problem in the ${Resources}`)
+									done(new Error("either device is not registered or reachable or both"));
+								})
+							}  
+						})
+					}	
+				//}
+			}
 		})
+	})
 })
 
