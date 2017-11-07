93a1437a679bd97f35abc9ad6ee23e5a17279b0f
bhoopesh <bhoopesh@izuma.net>
2017-11-07 14:44:38 +0530

    testing switching facade on relay

--- automate/stateproperty.js
+++ automate/stateproperty.js
@@ -6,24 +6,24 @@
  * This file for the test report 
  */
 "use strict"
-module.exports = function(stateproperty,setvalue,Resources,facades){
+module.exports = function(stateproperty,setvalue,deviceId,facades){
 	return new Promise(function(resolve, reject) {
-		dev$.selectByID(Resources).set(stateproperty, setvalue).then(function(setResp) {
-			if(setResp && setResp[Resources] && setResp[Resources].receivedResponse && setResp[Resources].response.error === null) {
+		dev$.selectByID(deviceId).set(stateproperty, setvalue).then(function(setResp) {
+			if(setResp && setResp[deviceId] && setResp[deviceId].receivedResponse && setResp[deviceId].response.error === null) {
 				//Successfuly set the power to 'on'
-				dev$.selectByID(Resources).get(stateproperty).then(function(getResp) {
-					if(getResp && getResp[Resources] && getResp[Resources].response && typeof getResp[Resources].response.result !== 'undefined') {
-						if(getResp[Resources].response.result == setvalue) {
-							//console.log(`device ${Resources} has facades- ${facades}`.blue)
-				console.log(/*'\tdevice:'.green,Resources ,'\n',
+				dev$.selectByID(deviceId).get(stateproperty).then(function(getResp) {
+					if(getResp && getResp[deviceId] && getResp[deviceId].response && typeof getResp[deviceId].response.result !== 'undefined') {
+						if(getResp[deviceId].response.result == setvalue) {
+							//console.log(`device ${deviceId} has facades- ${facades}`.blue)
+				console.log(/*'\tdevice:'.green,deviceId ,'\n',
 						'\ttesting facades:'.green,facades ,'\n',*/
 						'\t\tSet State:'.green,stateproperty ,'\n',
 						'\t\tValue:'.green,setvalue ,'\n',
 						'\t\t\tgetstate:'.green,stateproperty ,'\n',
-						'\t\t\tReceive responce:'.green,getResp[Resources].receivedResponse ,'\n',
-						'\t\t\tResponce result:'.green,getResp[Resources].response.result ,'\n',
-						'\t\t\tError:'.green,getResp[Resources].response.error)
-				//console.log('Tested Facade:'.green,`${facades} for the device ${Resources} successfully`.blue)
+						'\t\t\tReceive responce:'.green,getResp[deviceId].receivedResponse ,'\n',
+						'\t\t\tResponce result:'.green,getResp[deviceId].response.result ,'\n',
+						'\t\t\tError:'.green,getResp[deviceId].response.error)
+				//console.log('Tested Facade:'.green,`${facades} for the device ${deviceId} successfully`.blue)
 							resolve();
 							//Previous set successfully set the power value to 'on'
 						} else {
